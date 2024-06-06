d338428746d8b67e57aacf23cd3e6311c0f8dc93
himani771 <himanibhatt668@gmail.com>
2024-06-06 13:45:12 +0530

    Update DevOps_Jenkins_Example.java

--- DevOps_Jenkins_Example.java
+++ DevOps_Jenkins_Example.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class DevOpsExample {
+public class DevOps_Jenkins_Example {
   public static void main(String[] args) {
     System.out.println("Welcome to DevOps.");
   }
